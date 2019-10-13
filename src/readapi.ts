84f3026e58434091478588eafd01d820d9be838a
Otto Poikajärvi <tootsi666@gmail.com>
2019-10-13 19:17:01 +0300

    Added func module

---
+++ src/readapi.ts
@@ -0,0 +1,21 @@
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
+//This file is imported to the main plugin file
+//Includes functions that check security features of a OPENAPI-file
+
+function checkHTTP(doc) {
+    var servers = doc.servers;
+    for (var server of servers) {
+        var address = server['url'];
+        console.log(address);
+    }
+}
+
+try {
+    var ymlfile = yaml.safeLoad(fs.readFileSync('petstore_large.yaml', 'utf8'));
+} catch (e) {
+    console.log(e);
+}
+
+checkHTTP(ymlfile);
