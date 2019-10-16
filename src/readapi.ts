2cdc813755f46d2c0c7099683f99eb8e0a055216
Otto Poikajärvi <tootsi666@gmail.com>
2019-10-16 22:53:46 +0300

    Dict creation, checks https

--- src/readapi.ts
+++ src/readapi.ts
@@ -4,16 +4,24 @@ const fs   = require('fs');
 //This file is imported to the main plugin file
 //Includes functions that check security features of a OPENAPI-file
 
-function checkHTTP(doc) {
+function checkHTTP(doc: any) {
     var servers = doc.servers;
+    var addrlist: {[index: string]:any} = {};
     for (var server of servers) {
         var address = server['url'];
         console.log(address);
+        if (address.startsWith("https")){
+            addrlist[address] = true;
+        }
+        else {
+            addrlist[address] = false;
+        }
     }
+    console.log(addrlist);
 }
 
 try {
-    var ymlfile = yaml.safeLoad(fs.readFileSync('src/test/petstore.yaml', 'utf8'));
+    var ymlfile = yaml.safeLoad(fs.readFileSync('test/petstore.yaml', 'utf8'));
 } catch (e) {
     console.log(e);
 }
