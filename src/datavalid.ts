bce17a7106827ce09fcbee229e2737a07cc300a1
Otto Poikajärvi <tootsi666@gmail.com>
2019-11-08 13:28:18 +0200

    Init commit

---
+++ src/datavalid.ts
@@ -0,0 +1,11 @@
+//This file is imported to the main plugin file
+//Includes functions that check for data validation issues in the OPENAPI-contract
+
+export class Datavalidationcheck {
+
+    yaml: any;
+    constructor(doc: any) {
+        this.yaml = doc;
+    }
+
+}
