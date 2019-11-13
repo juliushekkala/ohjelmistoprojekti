f07b83f0c2139db9f338ee6e209f66ff2a805903
unknown <tootsi666@gmail.com>
2019-11-13 10:33:05 +0200

    Fix for schemas in params

--- src/datavalid.ts
+++ src/datavalid.ts
@@ -14,19 +14,30 @@ export class Datavalidationcheck {
 checkParamSchemas() {
     //Checks if each parameter object has a schema defined
     //Schemas limit accepted inputs (SQL injections)
+    // Does NOT recognize entirely missing parameters as an error, just missing schemas
     var paths = this.yaml.paths;
     var problemparams: {[index: string]:any} = {};
     problemparams['status'] = true;
     problemparams.locations = [];
     for (let path in paths) {
+        if (paths[path]['parameters'] !== undefined) {
+            //Parameter object itself in [0], reference should be [1]
+            if (paths[path]['parameters'][0]['schema'] === undefined) {
+                let location = "paths" + path +  "/" + "parameters";
+                problemparams.locations.push(location);
+                if (problemparams['status']) {
+                    problemparams['status'] = false;
+                }
+            }
+        }
         for (let itemobject in paths[path]) {
             if (paths[path][itemobject]['parameters'] === undefined) {
                 //No parameters here
                 continue;
             }
             else {
-                //TODO Checking if schema actually exists in list parameters
-                if (paths[path][itemobject]['parameters']['schema'] === undefined) {
+                //Parameter object itself in [0], reference should be [1]
+                if (paths[path][itemobject]['parameters'][0]['schema'] === undefined) {
                     let location = "paths" + path + "/" + itemobject + "/" + "parameters";
                     problemparams.locations.push(location);
                     if (problemparams['status']) {
